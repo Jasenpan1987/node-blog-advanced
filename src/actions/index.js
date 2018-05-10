9ed1a9c9514721e7c66824f33edcaa581d5d0482
Jasenpan1987 <jasenpan@gmail.com>
2018-05-10 20:59:29 +1000

    final

---
+++ src/actions/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from './types';
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token); // {key, url}
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitBlog = (values, file, history) => async dispatch => {
+  const uploadConfig = await axios.get("/api/upload"); // get s3 upload url
+
+  const upload = await axios.put(uploadConfig.data.url, file, {
+    headers: {
+      "Content-Type": file.type
+    }
+  });
+  const res = await axios.post('/api/blogs', { ...values, imgUrl: uploadConfig.data.key });
+
+  history.push('/blogs');
+  dispatch({ type: FETCH_BLOG, payload: res.data });
+};
+
+export const fetchBlogs = () => async dispatch => {
+  const res = await axios.get('/api/blogs');
+
+  dispatch({ type: FETCH_BLOGS, payload: res.data });
+};
+
+export const fetchBlog = id => async dispatch => {
+  const res = await axios.get(`/api/blogs/${id}`);
+
+  dispatch({ type: FETCH_BLOG, payload: res.data });
+};
